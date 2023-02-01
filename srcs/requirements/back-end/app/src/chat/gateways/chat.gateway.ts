784bb2cc969a39e1800a614dd15452988f07b774
Gustave MARY <gmary@e2r3p12.clusters.42paris.fr>
2023-02-01 10:02:20 +0100

    progress on backend 2 gus

--- srcs/requirements/back-end/app/src/chat/gateways/chat.gateway.ts
+++ srcs/requirements/back-end/app/src/chat/gateways/chat.gateway.ts
@@ -65,25 +65,89 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 	}
 
 	@SubscribeMessage('joinRoom')
-	handleJoinRoom(socket: Socket, data: string): WsResponse<unknown> {
-		socket.join(data);
-		
+	handleJoinRoom(socket: Socket, room: string): WsResponse<unknown> {
+		//! ici on va ajouter a prisma la room dans la table user
+		//? on peut peut etre emit un petit message pour dire quon a join la room
+		return { event: 'joinRoom', data: room};
 	}
 
 	@SubscribeMessage('leaveRoom')
-	handleLeaveRoom(socket: Socket, data: string): WsResponse<unknown> {
-		socket.leave(data);
+	handleLeaveRoom(socket: Socket, room: string): WsResponse<unknown> {
+		//? emit un  petit message pour dire que tel user a quitte la room
+		//! ici on va ajouter a prisma la room dans la table user
+		socket.leave(room);
+		return { event: 'joinRoom', data: room};
 	}
 
 	@SubscribeMessage('createRoom')
-	handleCreateRoom(socket: Socket, data: string): WsResponse<unknown> {
+	handleCreateRoom(socket: Socket, room: string): WsResponse<unknown> {
+		//* usr devient admin
+		//! ici on va ajouter a prisma la room dans la table user
+		socket.join(room);
+		return { event: 'createRoom', data: room};
 	}
 
+	@SubscribeMessage('deleteRoom')
+	handleDeleteRoom(socket: Socket, room: string): void {
+		//* check si le user est admin
+		socket.leave(room);
+		//! ici on va supprimer la room dans la table user de prisma
+	}
+
+	@SubscribeMessage('banUser')
+	handleBanUser(socket: Socket, user: string): void {
+		//* check si le user est admin
+		//! changer prisma pour que le user soit ban
+		//? emit un petit message pour dire que tel user a ete ban
+	}
+
+	@SubscribeMessage('unbanUser')
+	handleUnbanUser(socket: Socket, user: string): void {
+		//* check si le user est admin
+		//! changer prisma pour que le user soit unban
+		//? emit un petit message pour dire que tel user a ete unban
+	}
+
+	@SubscribeMessage('blockUser')
+	handleBlockUser(socket: Socket, user: string): void {
+		//! changer prisma pour que le user soit block
+	}
+
+	@SubscribeMessage('unblockUser')
+	handleUnblockUser(socket: Socket, user: string): void {
+		//! changer prisma pour que le user soit unblock
+	}
+
+	@SubscribeMessage('muteUser')
+	handleMuteUser(socket: Socket, user: string): void {
+		//! changer prisma pour que le user soit mute
+	}
+
+	@SubscribeMessage('unmuteUser')
+	handleUnmuteUser(socket: Socket, user: string): void {
+		//! changer prisma pour que le user soit unmute
+	}
+
+	@SubscribeMessage('addAdmin')
+	handleAddAdmin(socket: Socket, user: string): void {
+		//* check si le user qui fait la demande est admin
+		//! changer prisma pour que le user soit admin
+		//? emit un petit message pour dire que tel user est admin
+	}
+
+	@SubscribeMessage('removeAdmin')
+	handleRemoveAdmin(socket: Socket, user: string): void {
+		//* check si le user qui fait la demande est admin
+		//! changer prisma pour que le user soit plus admin
+		//? emit un petit message pour dire que tel user n'est plus admin
+	}
+		
+
 	@SubscribeMessage('message') // Subscribe to the message event send by the client (front end) called 'message'
-	handleMessage(@MessageBody() message: unknown): WsResponse<unknown> {
+	handleMessage(@MessageBody() message: unknown): void {
 		// handleMessage(@MessageBody() message: string): void {
 		// console.log('C est le message : ', message);
-		message = message + ' parasite';
+		// message = message + ' parasite';
 		// this.myserver.to
 		
 		this.myserver.emit('message', message); // Emit the message event to the client, for every user
